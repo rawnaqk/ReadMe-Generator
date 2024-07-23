ab399cb01751a03a4a7f080a0cbad9ec47f9689a
rawnaqk <rawnaq.kabairzad@gmail.com>
2024-07-23 18:46:02 -0400

    finished generateMarkdown code

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,67 @@
+// Function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    if (license === 'None') {
+      return '';
+    }
+    return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  
+  // Function that returns the license link
+  // If there is no license, return an empty string
+  function renderLicenseLink(license) {
+    if (license === 'None') {
+      return '';
+    }
+    return `[License](#license)`;
+  }
+  
+  // Function that returns the license section of README
+  // If there is no license, return an empty string
+  function renderLicenseSection(license) {
+    if (license === 'None') {
+      return '';
+    }
+    return `## License
+  This project is licensed under the ${license} license.`;
+  }
+  
+  // Function to generate markdown for README
+  function generateMarkdown(data) {
+    return `
+  # ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - ${renderLicenseLink(data.license)}
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  For questions about the project, please contact me via GitHub: [${data.github}](https://github.com/${data.github})  
+  or email me at: ${data.email}
+  `;
+  }
+  
+  module.exports = generateMarkdown;
